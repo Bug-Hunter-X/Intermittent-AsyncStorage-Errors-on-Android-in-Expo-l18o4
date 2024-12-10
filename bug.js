bee9f5419ba2e1eab4c3688723940f178a0cb826
BugHunter-X <askzenai@gmail.com>
2024-12-10 04:56:08 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This error occurs when using AsyncStorage in Expo and occurs only on Android. The error message is not very descriptive and can be difficult to track down, especially when it happens only sometimes. It might manifest as a failure to save data to AsyncStorage, or it might return null or undefined data unexpectedly, even if data was written earlier.
